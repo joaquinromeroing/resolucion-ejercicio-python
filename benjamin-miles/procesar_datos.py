f8bf3ee9d85551ba85fbb498224cc92ae9c8959e
BMilesRouillon <bmiles@mail.austral.edu.ar>
2026-04-08 15:28:56 +0000

    agrega menu y ordenamiento condicional al procesamiento

--- benjamin-miles/procesar_datos.py
+++ benjamin-miles/procesar_datos.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 def bubble_sort(data, col_index):
     n = len(data)
-    
+
     for i in range(n):
         for j in range(0, n - i - 1):
             if data[j][col_index] > data[j + 1][col_index]:
                 data[j], data[j + 1] = data[j + 1], data[j]
-    
+
     return data
 
 
@@ -15,33 +15,36 @@ def bubble_sort(data, col_index):
 path = input("Indique el path del csv: ")
 ordenado = input("El archivo está ordenado? (Y/N): ").upper()
 
+while ordenado not in ["Y", "N"]:
+    ordenado = input("Opción inválida. Ingrese Y o N: ").upper()
 
-# LECTURA 
-df = pd.read_csv(path)
+
+# LECTURA
+df = pd.read_csv(path, sep=";")
 
 
-# ORDENAMIENTO 
+# ORDENAMIENTO
 if ordenado == "N":
-    
+
     data = df.values.tolist()
     columnas = df.columns
-    
+
     col_index = df.columns.get_loc("PRSUC")
-    
+
     data_ordenada = bubble_sort(data, col_index)
-    
+
     df = pd.DataFrame(data_ordenada, columns=columnas)
-    
-    df.to_csv("archivo_ordenado.csv", index=False)
-    
-    print("Archivo ordenado generado.")
+
+    df.to_csv("archivo_temporal_ordenado.csv", index=False, sep=";")
+
+    print("Archivo temporal ordenado generado. Se continúa la ejecución con ese archivo.")
 
 
-# CALCULO IMPORTE 
+# CALCULO IMPORTE
 df["PRIMPORTE"] = df["PRCANT"] * df["PRPRE"]
 
 
-# CORTE DE CONTROL 
+# CORTE DE CONTROL
 i = 0
 n = len(df)
 
@@ -49,19 +52,19 @@ resultados = []
 total_general = 0
 
 while i < n:
-    
-    sucursal = df.loc[i, 'PRSUC']
+
+    sucursal = df.loc[i, "PRSUC"]
     total_sucursal = 0
-    
-    while i < n and df.loc[i, 'PRSUC'] == sucursal:
-        total_sucursal += df.loc[i, 'PRIMPORTE']
+
+    while i < n and df.loc[i, "PRSUC"] == sucursal:
+        total_sucursal += df.loc[i, "PRIMPORTE"]
         i += 1
-    
+
     resultados.append([sucursal, total_sucursal])
     total_general += total_sucursal
 
 
-# RESULTADOS 
+# RESULTADOS
 df_resultados = pd.DataFrame(resultados, columns=["Sucursal", "Total Comprado"])
 
 print("\nResultados por sucursal:")
