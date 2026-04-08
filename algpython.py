7ca27d8cfc007b2c7daf33b0ef2779079e830940
FranB2MIL <franciscojbollea@gmail.com>
2026-04-08 19:14:55 -0300

    Added the Order function to main Algorithm (Observations: el corte de control funciona igualmente con el archivo desordenado que nos dio el profesor. Ya que las sucursales siguen agrupadas. No afecta que esten agrupadas pero desordenadas "alfabeticamente".)

--- algpython.py
+++ algpython.py
@@ -1,6 +1,50 @@
 import csv
 
-archivo = open("compras_ordenado.csv", "r")
+def orderList(archive):
+    archivo_for_open = open(archive, "r")
+
+    reader = csv.reader(archivo_for_open)
+    next(reader)  
+
+    datos = list(reader)
+
+    archivo_for_open.close()
+
+    n = len(datos)
+
+    for i in range(n):
+        for j in range(0, n-i-1):
+            # chequea condicion y hace intercambio si lo necesita
+            if datos[j][0] > datos[j+1][0]:
+                temp = datos[j]
+                datos[j] = datos[j + 1]
+                datos[j + 1] = temp
+
+    #una ves que la lista datos esta ordenada lo escribe en el nuevo archivo
+    archivo_out = open("compras_ordenado.csv", "w", newline="")
+    writer = csv.writer(archivo_out)
+
+    #escribo la primera linea de indices
+    writer.writerow(["PRSUC","PRCOD","PRFEC","PRPROV","PRCANT","PRPRE"])
+
+    #escribo los datos ordenados
+    writer.writerows(datos)
+
+    #cierro 
+    archivo_out.close()
+    
+print("Ingrese el nombre del archivo a ordenar (con extension .csv):")
+
+archivo_nombre = input()
+
+print("El archivo esta ordenado? (s/n)")    
+respuesta = input()
+
+if respuesta.lower() == 'n':
+    orderList(archivo_nombre)
+
+
+archivo = open(archivo_nombre, "r")
 reader = csv.reader(archivo)
 
 # leer segunda linea xq la primera no tiene datos.
