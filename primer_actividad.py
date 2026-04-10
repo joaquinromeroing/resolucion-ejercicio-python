c9073cc992dbf74c025af110fc3160f1631d3f2d
Ines Medina <inesmed004@gmail.com>
2026-04-10 02:59:30 +0000

    Paso 2: integración del ordenamiento con menú interactivo

--- primer_actividad.py
+++ primer_actividad.py
@@ -1,4 +1,48 @@
-archivo = open("COMPRAS_supermercado.csv", "r")
+def ordenar_archivo_si_es_necesario(path, ordenado):
+
+    if ordenado.upper() == "N":
+        print("Ordenando archivo...")
+
+        archivo = open(path, "r")
+        encabezado = archivo.readline()
+        datos = []
+
+        for linea in archivo:
+            datos.append(linea.strip().split(","))
+
+        archivo.close()
+
+        # ORDENAMIENTO BURBUJA POR SUCURSAL
+        n = len(datos)
+        for i in range(n):
+            for j in range(0, n - i - 1):
+                if datos[j][0] > datos[j + 1][0]:
+                    aux = datos[j]
+                    datos[j] = datos[j + 1]
+                    datos[j + 1] = aux
+
+        nuevo_path = "archivo_ordenado_temp.csv"
+        archivo = open(nuevo_path, "w")
+        archivo.write(encabezado)
+
+        for fila in datos:
+            archivo.write(",".join(fila) + "\n")
+
+        archivo.close()
+
+        return nuevo_path
+
+    else:
+        return path
+
+path = input("Ingrese el path del archivo CSV: ")
+ordenado = input("El archivo está ordenado? (Y/N): ")
+
+path_final = ordenar_archivo_si_es_necesario(path, ordenado)
+
+
+
+archivo = open(path_final, "r")
 archivo.readline()
 linea = archivo.readline()
 
@@ -10,35 +54,35 @@ while linea != "":
     PRSUC = datos[0]
     sucursal_actual = PRSUC
 
-    TOTSUC = 0 # total comprado en unidades
+    TOTSUC = 0
     max_importe = -1
     min_importe = 9999999999
-    MYPROD = ""   #producto de mayor 
-    MNPRO = ""# producto de mayor compra en peso
+    MYPROD = ""
+    MNPRO = ""
 
     while linea != "" and PRSUC == sucursal_actual:
         producto_actual = datos[1]
         TOTPES = 0
         TOTUNI = 0
+
         while linea != "" and PRSUC == sucursal_actual and datos[1] == producto_actual:
-               PRCANT = int(datos[4])
-               PRPRE = float(datos[5])
+            PRCANT = int(datos[4])
+            PRPRE = float(datos[5])
 
-               importe = PRCANT * PRPRE
-               
-               TOTUNI=TOTUNI + PRCANT
-               TOTSUC = TOTSUC + PRCANT
-               TOTPES = TOTPES + importe # total comprado en peso
-               TOTALIMP = TOTALIMP + importe #compra total de todas las sucu
-               
-               linea = archivo.readline()
-            
-               if linea != "":
-                    datos = linea.strip().split(",")
-                    PRSUC = datos[0]
+            importe = PRCANT * PRPRE
+
+            TOTUNI = TOTUNI + PRCANT
+            TOTSUC = TOTSUC + PRCANT
+            TOTPES = TOTPES + importe
+            TOTALIMP = TOTALIMP + importe
+
+            linea = archivo.readline()
+
+            if linea != "":
+                datos = linea.strip().split(",")
+                PRSUC = datos[0]
+
         
-        CANSUC = CANSUC + 1 #total de sucursales 
-
         if TOTPES > max_importe:
             max_importe = TOTPES
             MYPROD = producto_actual
@@ -46,19 +90,22 @@ while linea != "":
         if TOTPES < min_importe:
             min_importe = TOTPES
             MNPRO = producto_actual
-            
-    
-    print("Sucursal:", sucursal_actual,
-        "Producto:", producto_actual,
-        "TOTUNI:", TOTUNI,
-        "TOTPES:", TOTPES)
-    print("Sucursal:", sucursal_actual,
-        "TOTSUC:", TOTSUC,
-        "MYPROD:",MYPROD, 
-        "MYIMPOR:", max_importe,
-        "MNPRO:",MNPRO, 
-        "MNIMPOR:", min_importe)
-    print("El total de sucursales del supermercado son:", CANSUC," ", "y"," ","La compra total en pesos de todas las sucursales es:", "$", TOTALIMP)
 
+    CANSUC = CANSUC + 1  
+
+    print("Sucursal:", sucursal_actual,
+          "Producto:", producto_actual,
+          "TOTUNI:", TOTUNI,
+          "TOTPES:", TOTPES)
+
+    print("Sucursal:", sucursal_actual,
+          "TOTSUC:", TOTSUC,
+          "MYPROD:", MYPROD,
+          "MYIMPOR:", max_importe,
+          "MNPRO:", MNPRO,
+          "MNIMPOR:", min_importe)
+
+print("El total de sucursales del supermercado son:", CANSUC,
+      "y la compra total en pesos es: $", TOTALIMP)
 
 archivo.close()
