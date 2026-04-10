4462b053efa4d5d6653ff9f14aaf04e2341d6754
VictoriaMarull <vmarull@mail.austral.edu.ar>
2026-04-10 15:55:48 -0300

    agregue menu

--- analisis.py
+++ analisis.py
@@ -1,8 +1,79 @@
 import pandas as pd
+import csv
 
-df = pd.read_csv("COMPRAS_supermercado_ordenado.csv")
 
-#por prod
+def ordenar_csv(path_entrada, path_salida):
+    archivo = open(path_entrada, "r")
+    lector = csv.reader(archivo)
+
+    encabezado = next(lector)
+
+    filas = []
+    for fila in lector:
+        filas.append(fila)
+
+    archivo.close()
+
+    n = len(filas)
+
+    for i in range(n - 1):
+        for j in range(n - 1):
+
+            f1 = filas[j]
+            f2 = filas[j + 1]
+
+            intercambiar = False
+
+            if f1[0] > f2[0]:
+                intercambiar = True
+
+            elif f1[0] == f2[0]:
+                if f1[1] > f2[1]:
+                    intercambiar = True
+
+                elif f1[1] == f2[1]:
+                    if f1[2] > f2[2]:
+                        intercambiar = True
+
+                    elif f1[2] == f2[2]:
+                        if f1[3] > f2[3]:
+                            intercambiar = True
+
+                        elif f1[3] == f2[3]:
+                            if int(f1[4]) > int(f2[4]):
+                                intercambiar = True
+
+                            elif int(f1[4]) == int(f2[4]):
+                                if float(f1[5]) > float(f2[5]):
+                                    intercambiar = True
+
+            if intercambiar:
+                aux = filas[j]
+                filas[j] = filas[j + 1]
+                filas[j + 1] = aux
+
+    archivo = open(path_salida, "w", newline="")
+    escritor = csv.writer(archivo)
+
+    escritor.writerow(encabezado)
+
+    for fila in filas:
+        escritor.writerow(fila)
+
+    archivo.close()
+
+
+path = input("Indique el path del csv: ")
+ordenado = input("El archivo esta ordenado? (Y/N): ")
+
+if ordenado == "N":
+    path_temporal = "archivo_temporal_ordenado.csv"
+    ordenar_csv(path, path_temporal)
+    path = path_temporal
+
+df = pd.read_csv(path)
+
+# por prod
 i = 0
 
 while i < len(df):
@@ -21,8 +92,7 @@ while i < len(df):
     print("")
 
 
-#por suc
-
+# por suc
 i = 0
 
 while i < len(df):
@@ -62,7 +132,7 @@ while i < len(df):
     print("")
 
 
-#totales
+# totales
 i = 0
 cansuc = 0
 totalimp = 0
