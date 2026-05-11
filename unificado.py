fe3ae4ef376a17d55a5be2e7f6b441b8fae07983
VictoriaMarull <vmarull@mail.austral.edu.ar>
2026-05-11 19:44:23 -0300

    modifique el archivo unificado

--- unificado.py
+++ unificado.py
@@ -6,24 +6,35 @@ import pandas as pd
 # ORDENAMIENTO DEL ARCHIVO
 
 
+print("Cargando archivo...")
+
 archivo = open("COMPRAS_supermercado_desordenado_solo_sucursal.csv", "r")
 lector = csv.reader(archivo)
 
 encabezado = next(lector)
 
 filas = []
+
 for fila in lector:
     filas.append(fila)
 
 archivo.close()
 
+print("Archivo cargado.")
+print("Comenzando ordenamiento...")
 
-# ORDENAMIENTO BURBUJA
+
+
+# ORDENAMIENTO BURBUJA OPTIMIZADO
+
 
 n = len(filas)
 
 for i in range(n - 1):
-    for j in range(n - 1):
+
+    hubo_intercambio = False
+
+    for j in range(n - 1 - i):
 
         f1 = filas[j]
         f2 = filas[j + 1]
@@ -34,32 +45,48 @@ for i in range(n - 1):
             intercambiar = True
 
         elif f1[0] == f2[0]:
+
             if f1[1] > f2[1]:
                 intercambiar = True
 
             elif f1[1] == f2[1]:
+
                 if f1[2] > f2[2]:
                     intercambiar = True
 
                 elif f1[2] == f2[2]:
+
                     if f1[3] > f2[3]:
                         intercambiar = True
 
                     elif f1[3] == f2[3]:
+
                         if int(f1[4]) > int(f2[4]):
                             intercambiar = True
 
                         elif int(f1[4]) == int(f2[4]):
+
                             if float(f1[5]) > float(f2[5]):
                                 intercambiar = True
 
         if intercambiar:
+
             aux = filas[j]
             filas[j] = filas[j + 1]
             filas[j + 1] = aux
 
+            hubo_intercambio = True
+
+    if hubo_intercambio == False:
+        break
+
+
+print("Ordenamiento terminado.")
+
+
 
 # GUARDAR ARCHIVO ORDENADO
+
 
 archivo = open("COMPRAS_supermercado_ordenado.csv", "w", newline="")
 escritor = csv.writer(archivo)
@@ -71,12 +98,12 @@ for fila in filas:
 
 archivo.close()
 
-print("Archivo ordenado.")
+print("Archivo ordenado guardado.")
 print("")
 
 
 
-# LECTURA DEL ARCHIVO ORDENADO
+# LEER ARCHIVO ORDENADO
 
 
 df = pd.read_csv("COMPRAS_supermercado_ordenado.csv")
@@ -86,9 +113,13 @@ df = pd.read_csv("COMPRAS_supermercado_ordenado.csv")
 # CORTE DE CONTROL POR PRODUCTO
 
 
+print("CORTE POR PRODUCTO")
+print("")
+
 i = 0
 
 while i < len(df):
+
     suc = df["PRSUC"][i]
     prod = df["PRCOD"][i]
 
@@ -96,47 +127,64 @@ while i < len(df):
     totpes = 0
 
     while i < len(df) and df["PRSUC"][i] == suc and df["PRCOD"][i] == prod:
+
         totuni = totuni + df["PRCANT"][i]
         totpes = totpes + df["PRCANT"][i] * df["PRPRE"][i]
+
         i = i + 1
 
-    print("Sucursal:", suc, "| Producto:", prod, "| Unidades:", totuni, "| Total $:", totpes)
-    print("")
+    print("Sucursal:", suc,
+          "| Producto:", prod,
+          "| Unidades:", totuni,
+          "| Total $:", totpes)
+
+print("")
 
 
 
 # CORTE DE CONTROL POR SUCURSAL
 
 
+print("CORTE POR SUCURSAL")
+print("")
+
 i = 0
 
 while i < len(df):
+
     suc = df["PRSUC"][i]
 
     totsuc = 0
+
     myprod = ""
     myimpor = 0
+
     mnprod = ""
     mnimpor = 999999999
 
     while i < len(df) and df["PRSUC"][i] == suc:
+
         prod = df["PRCOD"][i]
 
         totuni = 0
         totpes = 0
 
         while i < len(df) and df["PRSUC"][i] == suc and df["PRCOD"][i] == prod:
+
             totuni = totuni + df["PRCANT"][i]
             totpes = totpes + df["PRCANT"][i] * df["PRPRE"][i]
+
             i = i + 1
 
         totsuc = totsuc + totuni
 
         if totpes > myimpor:
+
             myimpor = totpes
             myprod = prod
 
         if totpes < mnimpor:
+
             mnimpor = totpes
             mnprod = prod
 
@@ -151,16 +199,24 @@ while i < len(df):
 # TOTALES GENERALES
 
 
+print("TOTALES GENERALES")
+print("")
+
 i = 0
+
 cansuc = 0
 totalimp = 0
 
 while i < len(df):
+
     suc = df["PRSUC"][i]
+
     cansuc = cansuc + 1
 
     while i < len(df) and df["PRSUC"][i] == suc:
+
         totalimp = totalimp + df["PRCANT"][i] * df["PRPRE"][i]
+
         i = i + 1
 
 print("Cantidad de sucursales:", cansuc)
